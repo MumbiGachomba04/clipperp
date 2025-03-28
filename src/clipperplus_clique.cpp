e084b372b3dce7094062672bf835752dc077fbec
Regina Mumbi Gachomba <mumbigachomba254@gmail.com>
2025-03-28 14:16:15 +0100

    all procs know max clique

--- src/clipperplus_clique.cpp
+++ src/clipperplus_clique.cpp
@@ -14,7 +14,7 @@ std::pair<std::vector<Node>, CERTIFICATE> parallel_find_clique(const Graph &grap
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int num_vertices = graph.size();
-    int num_parts = size;  
+    int num_parts = size;  // Each process handles one partition
     std::vector<idx_t> partition(num_vertices, 0);
     idx_t objval;
     idx_t ncon = 1;
@@ -22,7 +22,7 @@ std::pair<std::vector<Node>, CERTIFICATE> parallel_find_clique(const Graph &grap
     std::vector<idx_t> xadj(num_vertices + 1, 0);
     std::vector<idx_t> adjncy;
 
-    // Master process partitions the graph
+    // Master process (rank 0) partitions the graph
     if (rank == 0) {
         for (int i = 0; i < num_vertices; ++i) {
             const auto &neighbors = graph.neighbors(i);
@@ -43,10 +43,10 @@ std::pair<std::vector<Node>, CERTIFICATE> parallel_find_clique(const Graph &grap
         }
     }
 
-    // Broadcast partitioning info
+    // Broadcast partitioning information to all processes
     MPI_Bcast(partition.data(), num_vertices, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Each process extracts its nodes
+    // Each process determines its subset of nodes
     std::vector<Node> local_nodes;
     for (int i = 0; i < num_vertices; ++i) {
         if (partition[i] == rank) {
@@ -54,39 +54,57 @@ std::pair<std::vector<Node>, CERTIFICATE> parallel_find_clique(const Graph &grap
         }
     }
 
-    // Create induced subgraph
+    // Create the induced subgraph for each process
     Graph local_graph = graph.induced(local_nodes);
 
-    // Compute clique in local subgraph
+    // Each process finds the max clique in its subgraph
     auto local_result = find_clique(local_graph);
     int local_clique_size = local_result.first.size();
 
-    // Gather all clique sizes at rank 0
-    std::vector<int> all_clique_sizes(size);
-    MPI_Allgather(&local_clique_size, 1, MPI_INT, all_clique_sizes.data(), 1, MPI_INT, MPI_COMM_WORLD);
-
-    // Rank 0 determines the best clique
-    int best_rank = std::max_element(all_clique_sizes.begin(), all_clique_sizes.end()) - all_clique_sizes.begin();
-    int best_size = all_clique_sizes[best_rank];
+    // Gather all clique sizes at rank 0 to find the maximum
+    int best_size;
+    MPI_Allreduce(&local_clique_size, &best_size, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
 
     std::vector<Node> best_clique;
     CERTIFICATE final_certificate;
 
-    if (rank == best_rank) {
-        // The process with the largest clique sends it to rank 0
-        MPI_Send(local_result.first.data(), best_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        best_clique = local_result.first;
+        final_certificate = local_result.second;
+
+        for (int i = 1; i < size; ++i) {
+            int recv_size;
+            MPI_Recv(&recv_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            int recv_cert;
+            MPI_Recv(&recv_cert, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            CERTIFICATE certificate = static_cast<CERTIFICATE>(recv_cert);
+
+            if (recv_size == best_size) {
+                std::vector<Node> clique(recv_size);
+                MPI_Recv(clique.data(), recv_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                best_clique = clique;
+                final_certificate = certificate;
+            }
+        }
+    } else {
+        MPI_Send(&local_clique_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
         int cert = static_cast<int>(local_result.second);
-        MPI_Send(&cert, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+        MPI_Send(&cert, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        if (local_clique_size == best_size) {
+            MPI_Send(local_result.first.data(), local_clique_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
     }
 
+    // Broadcast final best clique and certificate to all processes
+    int best_cert;
     if (rank == 0) {
-        // Rank 0 receives the best clique
-        best_clique.resize(best_size);
-        MPI_Recv(best_clique.data(), best_size, MPI_INT, best_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        int cert;
-        MPI_Recv(&cert, 1, MPI_INT, best_rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        final_certificate = static_cast<CERTIFICATE>(cert);
+        best_cert = static_cast<int>(final_certificate);
     }
+    MPI_Bcast(&best_cert, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    final_certificate = static_cast<CERTIFICATE>(best_cert);
+
+    best_clique.resize(best_size);
+    MPI_Bcast(best_clique.data(), best_size, MPI_INT, 0, MPI_COMM_WORLD);
 
     return {best_clique, final_certificate};
 }
