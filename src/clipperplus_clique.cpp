a1e24f24133a98fa98d7346921f7a608d9a394ba
Regina Mumbi Gachomba <mumbigachomba254@gmail.com>
2025-05-14 13:00:32 +0200

    random partitioning

--- src/clipperplus_clique.cpp
+++ src/clipperplus_clique.cpp
@@ -30,17 +30,30 @@ std::pair<std::vector<Node>, CERTIFICATE> parallel_find_clique(const Graph &grap
             adjncy.insert(adjncy.end(), neighbors.begin(), neighbors.end());
         }
 
-        idx_t options[METIS_NOPTIONS];
-        METIS_SetDefaultOptions(options);
-        options[METIS_OPTION_UFACTOR] = 500; 
+        int local_size=num_vertices/num_parts;
+        int rem = num_vertices%num_parts;
+        int count= 1;
+        int part_count=0;
 
-        int status = METIS_PartGraphKway(&num_vertices, &ncon, xadj.data(), adjncy.data(),
-                                         nullptr, nullptr, nullptr, &num_parts, 
-                                         nullptr, nullptr, options, &objval, partition.data());
-
-        if (status != METIS_OK) {
-            throw std::runtime_error("METIS partitioning failed");
+        for (int i = 0; i < num_vertices; ++i) {
+            if (count%local_size == 0){     
+             part_count++;
+            }
+            partition[i] = part_count;
+            count++;
         }
+
+        // idx_t options[METIS_NOPTIONS];
+        // METIS_SetDefaultOptions(options);
+        // options[METIS_OPTION_UFACTOR] = 500; 
+
+        // int status = METIS_PartGraphKway(&num_vertices, &ncon, xadj.data(), adjncy.data(),
+        //                                  nullptr, nullptr, nullptr, &num_parts, 
+        //                                  nullptr, nullptr, options, &objval, partition.data());
+
+        // if (status != METIS_OK) {
+        //     throw std::runtime_error("METIS partitioning failed");
+        // }
     }
 
     // Broadcast partition to all processes . no need to send csr
