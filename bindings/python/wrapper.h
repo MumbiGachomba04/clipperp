ffef450ab27b07b9790035bdc62e4455c4cdd86c
Regina Mumbi Gachomba <mumbigachomba254@gmail.com>
2025-06-04 16:50:22 +0200

    added conditional for when mpi size =1

--- bindings/python/wrapper.h
+++ bindings/python/wrapper.h
@@ -12,12 +12,20 @@
 class Wrapper {
   public:
     static std::tuple<long, std::vector<int>, int> clipperplus_clique_wrapper(const Eigen::MatrixXd& adj){
+      int size;
+      MPI_Comm_size(MPI_COMM_WORLD, &size);
       
       MPI_Barrier(MPI_COMM_WORLD);
       double start_time = MPI_Wtime();
+      if (size > 1){
       auto [clique, certificate] = clipperplus::parallel_find_clique(adj);
+      }
+      else {
+        auto [clique, certificate] = clipperplus::find_clique(adj);
+      }
       MPI_Barrier(MPI_COMM_WORLD);
       double end_time = MPI_Wtime();
+      
       int rank;
       MPI_Comm_rank(MPI_COMM_WORLD, &rank);
       if (rank == 0) {
